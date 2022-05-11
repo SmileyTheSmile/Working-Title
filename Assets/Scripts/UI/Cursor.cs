5c54b372ff1d0df3c202999e50fe78684c3ccaf0
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2022-05-11 11:22:14 +0300

    Added circular clamping for aim

--- Assets/Scripts/UI/Cursor.cs
+++ Assets/Scripts/UI/Cursor.cs
@@ -7,9 +7,11 @@ public class Cursor : MonoBehaviour
     private Camera mainCamera;
     private PlayerInputHandler inputHandler;
 
+    [SerializeField] private ClampModes clampMode;
     [SerializeField] private Transform playerCenter;
     [SerializeField] private Transform target;
     [SerializeField] private Vector2 threshold;
+    [SerializeField] private float clampRadius;
 
     private Vector3 mousePosition;
     private Vector3 targetPosition;
@@ -38,14 +40,33 @@ public class Cursor : MonoBehaviour
 
     private void UpdateCameraTargetPosition()
     {
+        if (clampMode == ClampModes.rectangle)
+        {
+            ClampTargetInRect();
+        }
+        else if (clampMode == ClampModes.circle)
+        {
+            ClampTargetInCircle();
+        }
+    }
+
+    private void ClampTargetInRect()
+    {
         targetPosition = (playerCenter.position + mousePosition) / 2;
 
-        //targetPosition.x = Mathf.Clamp(targetPosition.x, playerCenter.position.x - threshold.x, playerCenter.position.x + threshold.x);
-        //targetPosition.y = Mathf.Clamp(targetPosition.y, playerCenter.position.y - threshold.y, playerCenter.position.y + threshold.y);
-        //target.position = targetPosition;
-        targetPosition = Vector3.ClampMagnitude(targetPosition, 2.0f);
+        targetPosition.x = Mathf.Clamp(targetPosition.x, playerCenter.position.x - threshold.x, playerCenter.position.x + threshold.x);
+        targetPosition.y = Mathf.Clamp(targetPosition.y, playerCenter.position.y - threshold.y, playerCenter.position.y + threshold.y);
 
-        target.position = target.position + targetPosition;
+        target.position = targetPosition;
+    }
+
+    private void ClampTargetInCircle()
+    {
+        targetPosition = (mousePosition - playerCenter.position) / 2;
+        
+        targetPosition = Vector3.ClampMagnitude(targetPosition, clampRadius);
+
+        target.position = playerCenter.position + targetPosition;
     }
 
     void OnDrawGizmos()
@@ -54,12 +75,25 @@ public class Cursor : MonoBehaviour
         UnityEditor.Handles.DrawDottedLine(target.position, transform.position, 1f);
         UnityEditor.Handles.DrawDottedLine(playerCenter.position, target.position, 1f);
 
-        UnityEditor.Handles.DrawWireDisc(targetPosition, Vector3.forward, 0.4f);
+        UnityEditor.Handles.DrawWireDisc(target.position, Vector3.forward, 0.4f);
         UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, 0.4f);
- 
-        UnityEditor.Handles.DrawLine(new Vector2(playerCenter.position.x - threshold.x, playerCenter.position.y + threshold.y), new Vector2(playerCenter.position.x + threshold.x, playerCenter.position.y + threshold.y));
-        UnityEditor.Handles.DrawLine(new Vector2(playerCenter.position.x - threshold.x, playerCenter.position.y + threshold.y), new Vector2(playerCenter.position.x - threshold.x, playerCenter.position.y - threshold.y));
-        UnityEditor.Handles.DrawLine(new Vector2(playerCenter.position.x + threshold.x, playerCenter.position.y + threshold.y), new Vector2(playerCenter.position.x + threshold.x, playerCenter.position.y - threshold.y));
-        UnityEditor.Handles.DrawLine(new Vector2(playerCenter.position.x + threshold.x, playerCenter.position.y - threshold.y), new Vector2(playerCenter.position.x - threshold.x, playerCenter.position.y - threshold.y));
+
+        if (clampMode == ClampModes.rectangle)
+        {
+            UnityEditor.Handles.DrawLine(new Vector2(playerCenter.position.x - threshold.x, playerCenter.position.y + threshold.y), new Vector2(playerCenter.position.x + threshold.x, playerCenter.position.y + threshold.y));
+            UnityEditor.Handles.DrawLine(new Vector2(playerCenter.position.x - threshold.x, playerCenter.position.y + threshold.y), new Vector2(playerCenter.position.x - threshold.x, playerCenter.position.y - threshold.y));
+            UnityEditor.Handles.DrawLine(new Vector2(playerCenter.position.x + threshold.x, playerCenter.position.y + threshold.y), new Vector2(playerCenter.position.x + threshold.x, playerCenter.position.y - threshold.y));
+            UnityEditor.Handles.DrawLine(new Vector2(playerCenter.position.x + threshold.x, playerCenter.position.y - threshold.y), new Vector2(playerCenter.position.x - threshold.x, playerCenter.position.y - threshold.y));
+        }
+        else if (clampMode == ClampModes.circle)
+        {
+            UnityEditor.Handles.DrawWireDisc(playerCenter.position, Vector3.forward, clampRadius);
+        }
     }
+}
+
+public enum ClampModes
+{
+    rectangle,
+    circle
 }
